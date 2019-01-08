eac78b59a58986dec24f3d291d0830c2c6d2385e
DL032 <prasad.pasalkar@definelabs.com>
2019-01-08 19:39:35 +0530

    working on city result screen

--- Automation 7 jan 2019/src/Xpath.java
+++ Automation 7 jan 2019/src/Xpath.java
@@ -21,7 +21,36 @@ public class Xpath {
 		
 		public static final String BOOK_TEE_TIME_CROSS = "//android.widget.ImageView[@resource-id='"+v+":id/ivClear']";
 		public static final String BOOK_TEE_TIME_EDIT_TEXT_BACK_ARROW = "//android.widget.ImageView[@resource-id='"+v+":id/ivSearch']";
-		public static final String TOP_TEE_TIME_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/deals_screen_header'][@text='Today’s Top Tee Times']";		
+		public static final String TOP_TEE_TIME_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/deals_screen_header'][@text='Today’s Top Tee Times']";
+		public static final String CALENDAR_SUNDAY_TEXT = "//android.widget.TextView[@text='Sun']";
+		public static final String CALENDAR_MONDAY_TEXT = "//android.widget.TextView[@text='Mon']";
+		public static final String CALENDAR_TUESDAY_TEXT = "//android.widget.TextView[@text='Tue']";
+		public static final String CALENDAR_WENESDAY_TEXT = "//android.widget.TextView[@text='Wed']";
+		public static final String CALENDAR_THURSDAY_TEXT = "//android.widget.TextView[@text='Thu']";
+		public static final String CALENDAR_FRIDAY_TEXT = "//android.widget.TextView[@text='Fri']";
+		public static final String CALENDAR_SATURDAY_TEXT = "//android.widget.TextView[@text='Sat']";
+		
+		public static final String NEXT_MONTH_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/tv_NextMonth']";
+		public static final String BOOK_TEE_TIME_NEW_DATE_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/tv_Date']";
+		
+		public static final String HOW_MANY_PLAYER_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/players_screen_title'][@text='How many players?']";
+		public static final String PLAYER_ONE = "//android.widget.TextView[@resource-id='"+v+":id/players_screen_single_player_view'][@text='One Player']";
+		public static final String PLAYER_TWO = "//android.widget.TextView[@resource-id='"+v+":id/players_screen_two_player_view'][@text='Two Players']";
+		public static final String PLAYER_THREE = "//android.widget.TextView[@resource-id='"+v+":id/players_screen_three_player_view'][@text='Three Players']";
+		public static final String PLAYER_FOUR = "//android.widget.TextView[@resource-id='"+v+":id/players_screen_four_player_view'][@text='Four Players']";
+		public static final String PLAYER_ALL = "//android.widget.TextView[@resource-id='"+v+":id/players_screen_all_player_view'][@text='All']";
+		public static final String DONE_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/done'][@text='Done']";
+		
+		public static final String DALLAS_CITY_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/item_search_name'][@text='Dallas, TX']";
+		
+		public static final String CITY_HEADER_TEXT = "//android.widget.TextView[@resource-id='"+v+":id/collapsing_header_text'][@text='Dallas, TX']";
+		
+		public static final String FAV_HEADER_ICON = "//android.widget.ImageView[@resource-id='"+v+":id/collapsing_header_fav_btn']";
+		public static final String ALERT_HEADER_ICON = "//android.widget.ImageView[@resource-id='"+v+":id/collapsing_header_price_btn']";
+		public static final String FILTER_HEADER_ICON = "//android.widget.ImageView[@resource-id='"+v+":id/filter_menu']";
+		public static final String CITY_MAP_VIEW_BUTTON = "//android.widget.Button[@resource-id='"+v+":id/cvMapViewTab']";
+
+		
 	}
 	
 	
